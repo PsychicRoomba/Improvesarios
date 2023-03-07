baa0ebd96d388c7bbcc7d9f5b59cfcfecadcc2f2
Stephen Brown <stephenbrownprimary@gmail.com>
2023-03-07 11:21:28 +0000

    Portfolio Website Better

---
+++ src/container/Work/Work.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+
+import './Work.scss'
+
+const Work = () => {
+  return (
+    <div>Work</div>
+  )
+}
+
+export default Work
